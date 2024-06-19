0f4699cc2495427e7390bc28f9f6569bdac83508
cnaegele <cnaegele@lausanne.ch>
2024-06-19 11:06:34 +0200

    composant acteur data

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <ActeurData 
+        acteurId="666"
+      >
+      </ActeurData>
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+import ActeurData from './components/ActeurData.vue'
+</script>
---
+++ src/components/ActeurData.vue
@@ -0,0 +1,15 @@
+<style scoped>
+
+</style>
+
+<template>
+  <h1>Bientôt, les données de l'acteur id: {{ acteurId }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  acteurId: String,
+})
+const acteurId = ref(props.acteurId)
+</script>
